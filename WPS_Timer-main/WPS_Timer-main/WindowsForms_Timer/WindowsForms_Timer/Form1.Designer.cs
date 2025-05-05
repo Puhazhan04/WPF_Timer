e9b335136ba00c039fc997cc88cccf73d116a84f
Puhazhan04 <puhalan4@gmail.com>
2025-05-05 12:22:59 +0200

    Add files via upload

---
+++ WPS_Timer-main/WPS_Timer-main/WindowsForms_Timer/WindowsForms_Timer/Form1.Designer.cs
@@ -0,0 +1,137 @@
+﻿namespace WindowsForms_Timer_ILBA24
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Erforderliche Designervariable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Verwendete Ressourcen bereinigen.
+        /// </summary>
+        /// <param name="disposing">True, wenn verwaltete Ressourcen gelöscht werden sollen; andernfalls False.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Vom Windows Form-Designer generierter Code
+
+        /// <summary>
+        /// Erforderliche Methode für die Designerunterstützung.
+        /// Der Inhalt der Methode darf nicht mit dem Code-Editor geändert werden.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.label1 = new System.Windows.Forms.Label();
+            this.start = new System.Windows.Forms.Button();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.btn_pause = new System.Windows.Forms.Button();
+            this.btn_weiter = new System.Windows.Forms.Button();
+            this.btn_reset = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 36F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(172, 44);
+            this.label1.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(323, 82);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "00:00:20";
+            // 
+            // start
+            // 
+            this.start.BackColor = System.Drawing.Color.DeepSkyBlue;
+            this.start.Font = new System.Drawing.Font("Microsoft Sans Serif", 26.1F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.start.Location = new System.Drawing.Point(229, 155);
+            this.start.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.start.Name = "start";
+            this.start.Size = new System.Drawing.Size(196, 79);
+            this.start.TabIndex = 1;
+            this.start.Text = "Start";
+            this.start.UseVisualStyleBackColor = false;
+            this.start.Click += new System.EventHandler(this.start_Click);
+            // 
+            // timer1
+            // 
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // btn_pause
+            // 
+            this.btn_pause.BackColor = System.Drawing.Color.Violet;
+            this.btn_pause.Font = new System.Drawing.Font("Microsoft Sans Serif", 26.1F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_pause.Location = new System.Drawing.Point(58, 245);
+            this.btn_pause.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.btn_pause.Name = "btn_pause";
+            this.btn_pause.Size = new System.Drawing.Size(224, 82);
+            this.btn_pause.TabIndex = 2;
+            this.btn_pause.Text = "Pause";
+            this.btn_pause.UseVisualStyleBackColor = false;
+            this.btn_pause.Click += new System.EventHandler(this.btn_pause_Click);
+            // 
+            // btn_weiter
+            // 
+            this.btn_weiter.BackColor = System.Drawing.Color.PaleGreen;
+            this.btn_weiter.Font = new System.Drawing.Font("Microsoft Sans Serif", 26.1F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_weiter.Location = new System.Drawing.Point(386, 245);
+            this.btn_weiter.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.btn_weiter.Name = "btn_weiter";
+            this.btn_weiter.Size = new System.Drawing.Size(219, 79);
+            this.btn_weiter.TabIndex = 3;
+            this.btn_weiter.Text = "Weiter";
+            this.btn_weiter.UseVisualStyleBackColor = false;
+            this.btn_weiter.Click += new System.EventHandler(this.btn_weiter_Click);
+            // 
+            // btn_reset
+            // 
+            this.btn_reset.BackColor = System.Drawing.Color.DeepPink;
+            this.btn_reset.Font = new System.Drawing.Font("Microsoft Sans Serif", 26.1F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_reset.Location = new System.Drawing.Point(229, 368);
+            this.btn_reset.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.btn_reset.Name = "btn_reset";
+            this.btn_reset.Size = new System.Drawing.Size(231, 79);
+            this.btn_reset.TabIndex = 4;
+            this.btn_reset.Text = "Reset";
+            this.btn_reset.UseVisualStyleBackColor = false;
+            this.btn_reset.Click += new System.EventHandler(this.btn_reset_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Cyan;
+            this.ClientSize = new System.Drawing.Size(683, 502);
+            this.Controls.Add(this.btn_reset);
+            this.Controls.Add(this.btn_weiter);
+            this.Controls.Add(this.btn_pause);
+            this.Controls.Add(this.start);
+            this.Controls.Add(this.label1);
+            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button start;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Button btn_pause;
+        private System.Windows.Forms.Button btn_weiter;
+        private System.Windows.Forms.Button btn_reset;
+    }
+}
+
---
+++ WPS_Timer-main/WPS_Timer-main/WindowsForms_Timer/WindowsForms_Timer/Form1.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.Design;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsForms_Timer_ILBA24
+{
+    public partial class Form1 : Form
+    {
+        public TimeSpan timerL;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void start_Click(object sender, EventArgs e)
+        {
+            timerL = new TimeSpan(0,0,20);
+            label1.Text= timerL.ToString(@"hh\:mm\:ss");
+            timer1.Start();
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            timerL= timerL.Subtract(TimeSpan.FromSeconds(1));
+            label1.Text = timerL.ToString(@"hh\:mm\:ss");
+            if (timerL.TotalSeconds <= 0)
+            {
+                timer1.Stop();
+            }
+        }
+
+        private void btn_pause_Click(object sender, EventArgs e)
+        {
+            timer1.Stop();
+            label1.Text= timerL.ToString(@"hh\:mm\:ss");
+        }
+
+        private void btn_weiter_Click(object sender, EventArgs e)
+        {
+            label1.Text = timerL.ToString(@"hh\:mm\:ss");
+            timer1.Start();
+        }
+
+        private void btn_reset_Click(object sender, EventArgs e)
+        {
+            timer1.Stop();
+            timerL = new TimeSpan(0, 0, 20);
+            label1.Text= timerL.ToString(@"hh\:mm\:ss");
+        }
+
+      
+    }
+}
